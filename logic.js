4e622909fa99f914bb30a5fab7758cc85152394b
ShreyasSridhar24 <ss649@buffalo.edu>
2022-11-05 14:49:59 -0400

    logic

---
+++ logic.js
@@ -0,0 +1,14 @@
+//TODO
+/*
+    ----
+INITIAL STEPS-
+    Get skill data from user and parse it into a dictionary.
+    Mathematically implement a way to get the number of days ahead that the user needs to start working
+    Find a way to read syllabi and get deadlines
+    ----
+CODE FUNCTIONS-
+    */
+console.log("HIIII");
+
+
+
