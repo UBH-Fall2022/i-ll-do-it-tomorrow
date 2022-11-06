b9c54efd74e9b0ae85d81cf00570e750366090de
ShreyasSridhar24 <ss649@buffalo.edu>
2022-11-06 07:13:57 -0500

    Nclass

--- init_login_read/logic.js
+++ init_login_read/logic.js
@@ -1,24 +1,25 @@
-//TODO
-/*
-    ----
-INITIAL STEPS-
-    Get skill data from user and parse it into a dictionary.
-    Add skill data to master dataset
-    Mathematically implement a way to get the number of days ahead that the user needs to start working
-    Find a way to read syllabi and get deadlines
-    ----
-CODE FUNCTIONS-
-    1) Implement a function csv_read(csv)-
-        Take data from a csv file and assign them to GLOBAL variables
-        Use that data in newer functions
-    2) Implement a function datamaker to add csvread to the dataset
-    2) Implement a function hours_calc(a,b,c)
-        Formulate a mathematical avg number of hours 
-    3) Implement a function days_calc(hours_calc,f1,f2)
-        Formulate a mathematical number of days using random values to differentiate data
+// //TODO
 
-    ----
-    */
+//     ----
+// INITIAL STEPS-
+//     Get skill data from user and parse it into a dictionary.
+//     Add skill data to master dataset
+//     Mathematically implement a way to get the number of days ahead that the user needs to start working
+//     Find a way to read syllabi and get deadlines
+//     Account for multiple subjects
+//     Create a dict of dates, and assign subjects to each day!
+//     ----
+// CODE FUNCTIONS-
+//     1) Implement a function csv_read(csv)-
+//         Take data from a csv file and assign them to GLOBAL variables
+//         Use that data in newer functions
+//     2) Implement a function datamaker to add csvread to the dataset
+//     2) Implement a function hours_calc(a,b,c)
+//         Formulate a mathematical avg number of hours 
+//     3) Implement a function days_calc(hours_calc,f1,f2)
+//         Formulate a mathematical number of days using random values to differentiate data
+
+//     ----
 
 //GLOBAL VARIABLES
 
@@ -31,7 +32,7 @@ credit_hrs=0
 js_skill=0;
 return_dict={}
 syll_dict={}
-
+numberOfClasses=0
 
 
 //Implement csv_read
@@ -47,7 +48,18 @@ function datamaker(survey_dict){
 }
 
 
-
+function numberOfClassesMaker(numberOfClasses){
+    if(survey_dict["Classes"]>=6 && csurvey_dict["Classes"]<14 ){
+        numberOfClasses+=2;
+    }
+    else if(csurvey_dict["Classes"]>=14 ){
+        numberOfClasses+=2;
+    }
+    else{
+        numberOfClasses+=1;
+    }
+    return numberOfClasses;
+}
 
 //find algorithmic quotient of ability
 function c_make(survey_dict, ai_determined_quotient){
@@ -60,11 +72,25 @@ function hours_calc(a,b,c){
     hours=(a+b)/2
     rconst=Math.random(1,3)+2^(Math.random(1,28)%3)+9/(2*Math.random(8,12))
     rconst+=Math.random((1,4))%2-1
-    hours+=rconst
-    hours=(hours+bias/2)
+    hours+=rconst*survey_dict["Proficiency[Python]"]
+    hours=((hours+c)/2)
+    return hours
 }
 
+function daySplit(){
+    reqDays=hours_calc(syll_dict["reqhrs"],master_dict[survey_dict],c_make)
+    daySplit1=(reqDays/4)+Math.random(2,2);
+    return daySplit
+}
 
+function monthDatasetMaker(daysDict){
+    dueDate=syll_dict["Due Date"]
+    startDate=dueDate-daySplit()
+   for (m of Range(0,daySplit)){
+        daysDict[m]="CODE TODAY!!!!!!!!!!!!!!!!!!!"
+   }
+
+}
 
 
 
